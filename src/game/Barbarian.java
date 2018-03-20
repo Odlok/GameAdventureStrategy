09528163cb42984be9012a33db7ecf90b0bc6261
Odlok <koldopellicer@opendeusto.es>
2018-03-20 11:01:53 +0100

    Minor Changes
    Esta es la prueba del Usuario 1

--- src/game/Barbarian.java
+++ src/game/Barbarian.java
@@ -5,13 +5,13 @@ public class Barbarian extends Character {
 	public Barbarian(WeaponBehaviour w) {
 		super(w);
 	}
-	
+
 	@Override
 	public void display() {
 	// TODO Auto-generated method stub
 
 		System.out.println("You are a strong, hulky barbarian. Añadimos texto en esta linea");
-		System.out.println("You are a strong, hulky barbarian. User 1 añade linea. Resolvemos conflicto directamente");
-		
+		System.out.println("You are a strong, hulky barbarian. Añadimos texto en esta linea. Prueba usuario 1");
+
 	}
 }
