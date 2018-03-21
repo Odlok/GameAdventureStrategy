4c6947b84fcad325545973e31a8b679b3ed00975
Odlok <koldopellicer@opendeusto.es>
2018-03-21 10:33:10 +0100

    Prueba 4

--- src/game/Barbarian.java
+++ src/game/Barbarian.java
@@ -11,14 +11,15 @@ public class Barbarian extends Character {
 	// TODO Auto-generated method stub
 
 		System.out.println("You are a strong, hulky barbarian. Añadimos texto en esta linea");
-<<<<<<< HEAD
+
 		System.out.println("You are a strong, hulky barbarian. Conflicto resuelto prueba Usuario 1");	
 		System.out.println("You are a strong, hulky barbarian. Prueba 2");	
-=======
+
 		System.out.println("You are a strong, hulky barbarian. Conflicto resuelto prueba Usuario 1");
 		System.out.println("You are a strong, hulky barbarian. Prueba nueva");
-		System.out.println("You are a strong, hulky barbarian. Prueba nueva 3");}
+		System.out.println("You are a strong, hulky barbarian. Prueba nueva 3");
+		
+		System.out.println("Si miras al abismo, el abismo te devolvera la mirada");}
 
->>>>>>> 8da18326bd0ddefdae59d93d5c4f16836ed9e218
 		}
 }
