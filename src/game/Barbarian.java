acada1df208d95dbce0ba9fddeeb89df5dd654c2
Odlok <koldopellicer@opendeusto.es>
2018-03-21 10:43:00 +0100

    Prueba de Koldo a User 1

--- src/game/Barbarian.java
+++ src/game/Barbarian.java
@@ -11,5 +11,7 @@ public class Barbarian extends Character {
 	// TODO Auto-generated method stub
 
 		System.out.println("You are a strong, hulky barbarian. Añadimos texto en esta linea");
-		System.out.println("You are a strong, hulky barbarian. Conflicto resuelto prueba Usuario 1 y 2");	}
+		System.out.println("You are a strong, hulky barbarian. Conflicto resuelto prueba Usuario 1 y 2");	
+		System.out.println("You are a strong, hulky barbarian. Prueba a User 1 XD");	}
+
 }
